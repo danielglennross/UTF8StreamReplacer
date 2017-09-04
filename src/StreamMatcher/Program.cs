0b3d83d7ad34f36e1acdbf67ebd44993cec9cd6c
danielglennross <danielglennross@gmail.com>
2017-09-04 14:50:08 +0100

    init commit, wip

---
+++ src/StreamMatcher/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace StreamMatcher
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input1 = "fghjfhghelloggfhdfghelldgfhellogh";
+            var bytes1 = Encoding.UTF8.GetBytes(input1);
+            var memoryStream1 = new MemoryStream();
+            var testStream1 = new UTf8StreamReplacer(memoryStream1, "hello", "daniel");
+
+            //testStream1.Write(bytes1, 3, 8);
+            //testStream1.Flush();
+            //var result2 = Encoding.UTF8.GetString(memoryStream1.ToArray());
+            //if (result2 == "jfhghell")
+            //{
+
+            //}
+
+            var chunks1 = Split(bytes1, 10);
+            foreach (var c in chunks1)
+            {
+                testStream1.Write(c, 0, c.Length);
+            }
+            testStream1.Flush();
+
+            var result1 = Encoding.UTF8.GetString(memoryStream1.ToArray());
+            if (result1 == "fghjfhgdanielggfhdfghelldgfdanielgh")
+            {
+
+            }
+
+            // test
+
+            var input = "tegdh%%{testing}}jgj%gd%%od%%{gfhj}}fgf";
+            var bytes = Encoding.UTF8.GetBytes(input);
+            var memoryStream = new MemoryStream();
+            var testStream = new UTf8StreamReplacer(memoryStream, (str) =>
+            {
+                if (str == "%%{testing}}") return "HelloWorld";
+                if (str == "%%{gfhj}}") return "Hello";
+                return null;
+            }, "%%{", "}}");
+
+            var chunks = Split(bytes, 10);
+            foreach (var c in chunks)
+            {
+                testStream.Write(c, 0, c.Length);
+            }
+            testStream.Flush();
+
+            var result = Encoding.UTF8.GetString(memoryStream.ToArray());
+            if (result == "tegdhHelloWorldjgj%gd%%odHellofgf")
+            {
+
+            }
+        }
+
+        private static IEnumerable<T[]> Split<T>(T[] array, int size)
+        {
+            for (var i = 0; i < (float)array.Length / size; i++)
+            {
+                yield return array.Skip(i * size).Take(size).ToArray();
+            }
+        }
+    }
+}
---
+++ src/StreamMatcher/UTf8StreamReplacer.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace StreamMatcher
+{
+    using StringReplacer = Replacer<string>;
+    using ByteReplacer = Replacer<byte[]>;
+
+    public delegate T Replacer<T>(T input);
+
+    public class UTf8StreamReplacer : Stream
+    {
+        private Stream _stream;
+        private byte[] _startDelimiter;
+        private byte[] _endDelimiter;
+        private Replacer<string> _stringReplacer;
+        private Replacer<byte[]> _byteReplacer;
+
+        private readonly byte[] _match;
+        private readonly byte[] _replace;
+
+        private readonly List<byte> _memoryStream = new List<byte>();
+        private readonly List<byte> _potentialStream = new List<byte>();
+
+        public UTf8StreamReplacer(Stream stream, string match, string replace)
+            : this(stream, Encoding.UTF8.GetBytes(match), Encoding.UTF8.GetBytes(replace))
+        { }
+
+        public UTf8StreamReplacer(Stream stream, byte[] match, byte[] replace)
+        {
+            _stream = stream;
+            _match = match;
+            _replace = replace;
+        }
+
+        public UTf8StreamReplacer(Stream stream, StringReplacer replacer, string delimiter)
+            : this(stream, replacer, delimiter, delimiter)
+        { }
+
+        public UTf8StreamReplacer(Stream stream, ByteReplacer replacer, byte[] delimiter)
+            : this(stream, replacer, delimiter, delimiter)
+        { }
+
+        public UTf8StreamReplacer(Stream stream, StringReplacer replacer, string startDelimiter, string endDelimiter)
+        {
+            Init(stream, replacer, Encoding.UTF8.GetBytes(startDelimiter), Encoding.UTF8.GetBytes(endDelimiter));
+        }
+
+        public UTf8StreamReplacer(Stream stream, ByteReplacer replacer, byte[] startDelimiter, byte[] endDelimiter)
+        {
+            Init(stream, replacer, startDelimiter, endDelimiter);
+        }
+
+        private void Init<T>(Stream stream, Replacer<T> replacer, byte[] startDelimiter, byte[] endDelimiter)
+        {
+            _stream = stream;
+            _startDelimiter = startDelimiter;
+            _endDelimiter = endDelimiter;
+
+            if (replacer is StringReplacer)
+            {
+                _stringReplacer = replacer as StringReplacer;
+            }
+            if (replacer is ByteReplacer)
+            {
+                _byteReplacer = replacer as ByteReplacer;
+            }
+        }
+
+        public override bool CanRead => true;
+
+        public override bool CanSeek => true;
+
+        public override bool CanWrite => true;
+
+        public override long Length => 0;
+
+        public override long Position { get; set; }
+
+        public override long Seek(long offset, SeekOrigin direction) => _stream.Seek(offset, direction);
+
+        public override void SetLength(long length) => _stream.SetLength(length);
+
+        public override void Close() => _stream.Close();
+
+        public override void Flush()
+        {
+            if (_potentialStream.Any())
+            {
+                _stream.Write(_potentialStream.ToArray(), 0, _potentialStream.Count);
+            }
+            _stream.Flush();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count) => _stream.Read(buffer, offset, count);
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            if (_startDelimiter != null && _endDelimiter != null)
+            {
+                WriteDelimited(buffer, offset, count);
+            }
+
+            if (_match != null && _replace != null)
+            {
+                WriteReplace(buffer, offset, count);
+            }
+        }
+
+        public void WriteReplace(byte[] buffer, int offset, int count)
+        {
+            Func<int, bool> isPotential = (currentBufferIndex) =>
+            {
+                for (var i = 0; i < _match.Length; i++)
+                {
+                    if (_potentialStream.Count == i && _match[i] == buffer[currentBufferIndex])
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            };
+
+            for (var i = offset; i < offset + count; i++)
+            {
+                if (isPotential(i))
+                {
+                    _potentialStream.Add(buffer[i]);
+                }
+                else
+                {// save potential, or normal
+                    if (_potentialStream.Any())
+                    {
+                        if (_potentialStream.SequenceEqual(_match))
+                        {// if matched
+                            _memoryStream.AddRange(_replace);
+                        }
+                        else
+                        {// didnt match, add what we got
+                            _memoryStream.AddRange(_potentialStream);
+                        }
+
+                        _potentialStream.Clear();
+                    }
+
+                    _memoryStream.Add(buffer[i]);
+                }
+            }
+
+            _stream.Write(_memoryStream.ToArray(), 0, _memoryStream.Count);
+            _memoryStream.Clear();
+        }
+
+        public void WriteDelimited(byte[] buffer, int offset, int count)
+        {
+            Func<int, bool> isPotential = (currentBufferIndex) =>
+            {
+                for (var i = 0; i < _startDelimiter.Length; i++)
+                {
+                    if (_potentialStream.Count == i && _startDelimiter[i] == buffer[currentBufferIndex])
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            };
+
+            Func<int, bool> isClosed = (currentBufferIndex) =>
+            {
+                if (_potentialStream.Count < _endDelimiter.Length)
+                {
+                    return false;
+                }
+
+                var eCount = _endDelimiter.Length - 1;
+                var pCount = _potentialStream.Count - 1;
+
+                for (var i = 0; i < _endDelimiter.Length; i++)
+                {
+                    if (_endDelimiter[eCount - i] != _potentialStream[pCount - i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            };
+
+            for (var i = offset; i < offset + count; i++)
+            {
+                if (_potentialStream.Count >= _startDelimiter.Length)
+                {// in segment
+                    _potentialStream.Add(buffer[i]);
+
+                    if (isClosed(i))
+                    {// end of segment
+
+                        var replacement = default(byte[]);
+                        if (_stringReplacer != null)
+                        {
+                            var potentialStr = Encoding.UTF8.GetString(_potentialStream.ToArray());
+                            var replacementStr = _stringReplacer(potentialStr);
+                            replacement = Encoding.UTF8.GetBytes(replacementStr);
+                        }
+
+                        if (_byteReplacer != null)
+                        {
+                            replacement = _byteReplacer(_potentialStream.ToArray());
+                        }
+
+                        _memoryStream.AddRange(replacement);
+                        _potentialStream.Clear();
+                    }
+                }
+                else if (isPotential(i))
+                {
+                    _potentialStream.Add(buffer[i]);
+                }
+                else
+                {// cancel potential, or normal
+                    if (_potentialStream.Any())
+                    {
+                        _memoryStream.AddRange(_potentialStream);
+                        _potentialStream.Clear();
+                    }
+
+                    _memoryStream.Add(buffer[i]);
+                }
+            }
+
+            _stream.Write(_memoryStream.ToArray(), 0, _memoryStream.Count);
+            _memoryStream.Clear();
+        }
+    }
+}
